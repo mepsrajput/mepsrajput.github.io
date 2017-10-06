6adafb51a16aebdf1217974ad24928d394cebffd
Pradeep Singh <viveksingh12126@gmail.com>
2017-10-06 19:57:50 +0530

    design chnges in about

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 	$(document).ready(function () {
 
-
-		$('.goTop').click(function () {
+	$('.goTop').click(function () {
 			$("html, body").animate({
 				scrollTop: 0
 			}, 1000);
 			return false;
 		});
 
+	function scrollNav() {
+		$('.nav a').click(function(){  
+			//Animate
+			$('html, body').stop().animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+			}, 800);
+		return false;
+	});
+	$('.scrollTop a').scrollTop();
+	}
+	scrollNav();
+	
 	});
